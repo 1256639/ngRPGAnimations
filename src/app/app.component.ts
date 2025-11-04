3ebc9da9fa2eda5beab16fb49d3eac6627425f39
Patrick Rolleston-Chase <1256639@cegepmontpetit.ca>
2025-11-04 13:53:30 -0500

    Exercice animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,66 @@
 import { Component } from '@angular/core';
 import {transition, trigger, useAnimation} from "@angular/animations";
+import {shakeX, pulse, jello} from "ng-animate";
+
+const DEATH_DURATION_SECONDS = 0.5;
+const ATTACK_PULSE_DURATION_SECONDS = 0.3;
+const PREATTACK_JELLO_DURATION_SECONDS = 0.5;
+const HIT_WOBBLE_DURATION_SECONDS = 0.3;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+
+  animations: [
+    trigger("death", [transition(":increment", useAnimation(shakeX, { params: { timing: DEATH_DURATION_SECONDS }}))]),
+    trigger("attack", [transition(":increment", useAnimation(pulse, { params: { timing: ATTACK_PULSE_DURATION_SECONDS, scale: 4.5 }}))]),
+    trigger("preAttack", [transition(":increment", useAnimation(jello, { params: { timing: PREATTACK_JELLO_DURATION_SECONDS }}))]),
+  ],
+
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   slimeIsPresent = false;
+
+  ng_death = 0;
+  ng_attack = 0;
+  ng_preAttack = 0;
+  css_hit = false;
 
   constructor() {
   }
 
   spawn() {
     this.slimeIsPresent = true;
-    // TODO Animation angular avec forwards
+    this.showSlime();
   }
 
   death(){
     this.slimeIsPresent = false;
-    // TODO Animation angular avec forwards
-
-    // TODO 2e animation angular en même temps
+    this.ng_death++;
+    this.hideSlime();
   }
 
   attack(){
-    // TODO Jouer une animation et augmenter l'intensité du mouvement avec scale
-    // TODO Jouer une autre animation avant
+    this.ng_preAttack++;
+    setTimeout(() => this.ng_attack++, 200);
   }
 
   hit(){
-    // TODO Utilisé Animista pour faire une animation différente avec css (wobble)
+    this.css_hit = true;
+    setTimeout(() => this.css_hit = false, HIT_WOBBLE_DURATION_SECONDS * 1000);
+  }
+
+  // Ajouter
+  showSlime() {
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeOut");
+    element?.classList.add("fadeIn");
+  }
+
+  hideSlime() {
+    var element = document.getElementById("slimeyId");
+    element?.classList.remove("fadeIn");
+    element?.classList.add("fadeOut");
   }
 }
